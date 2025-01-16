c2d5ba040d6e8eb26a52c925a93e9e47ea3c9af4
navya1220 <navyakalla1111@gmail.com>
2025-01-16 14:54:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -18,13 +18,25 @@ const app = express();
 app.use(express.json());
 
 // CORS configuration for the frontend
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://placement-portal-backend-e74c.onrender.com', 
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
+
 
 // MongoDB connection
 connectDB();
